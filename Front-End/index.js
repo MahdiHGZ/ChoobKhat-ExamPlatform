244a9011b8f99a705b07bd4f10dfce6142b4324f
MahdiHGZ <58703084+MahdiHGZ@users.noreply.github.com>
2020-07-25 02:04:06 +0430

    Update front:Cards

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,6 +1,13 @@
 
-var cards="";
+var cards ="";
 for (let i = 0; i < 100; i++) {
+
+    // if(i%6==0){
+    //   cards+=`
+    //   <div id="page-${((i-i%6)/6)+1}" class="col-12 rtl ppage">
+    //   `
+    // }
+
     if(i%2==0){
         var but_style="btn-primary"
         var but="اضافه شدن به آزمون"
@@ -17,29 +24,45 @@ for (let i = 0; i < 100; i++) {
               <p class="card-text">مخصوص پایه {exam_grade} </p>
               <p class="card-text">{qus_n}سواله</p>
               <p class="card-text">تاریخ برگزار: {exam_date}</p>
-              <pre class="card-text">{exam_dis}</pre>
-              <div class="text-left"><a id="btn-${i+1}" class="text-light btn ${but_style} btn-enter">${but}</a></div>
-
-            <div class="verify" id="verify-${i+1}">
-              <div class="input-group mb-3 ltr">
-                <div class="input-group-prepend">
-                  <button id="cbtm-${i+1}" class="btn btn-primary rounded-left border-3" type="button">برسی</button>
+              
+                <div id="token-error-${i+1}" class="error-token mb-2">
+                  <p class="card-text">{exam_dis}</p>
                 </div>
-                <input id="inp-${i+1}" type="text" class="form-control" placeholder="کد ورود" aria-label="" aria-describedby="basic-addon1">
-              </div>
-            </div>
-            </div>
+              
+              <div class="text-left"><a id="btn-${i+1}" class="text-light btn ${but_style} btn-enter">${but}</a></div>
+              <div class="verify" id="verify-${i+1}">
+                <div class="input-group mb-3 ltr">
+                  <div class="input-group-prepend">
+                    <button id="sbtm-${i+1}" class="btn btn-primary rounded-left border-3" type="button">برسی</button>
+                  </div>
+                   <input id="inp-${i+1}" type="text" class="form-control" placeholder="کد ورود" aria-label="" aria-describedby="basic-addon1">
+                </div>
+             </div>
             </div>
           </div>
+     </div>
     `
+    // if(i%6==0){
+    //   cards+=`</div>`
+    // }
 }
 
 $("#exam-cards").append(cards);
 
+
+
+
 for (let i = 0; i < 100; i++) {
     $(`#btn-${i+1}`).click(function(){
+      $(`.bg-error`).hide()
+      for (let j = 0; j < 100; j++){
+        $(`#token-error-${j+1}`).html(`
+              <p class="card-text">{exam_dis}</p>
+          `)
+      }
+      $(".btn-enter").show()
+      
         if(1) {
-            $(".btn-enter").show()
             $(".verify").hide()
             $(this).hide();
             $(`#verify-${i+1}`).css("display","inline");
@@ -49,3 +72,19 @@ for (let i = 0; i < 100; i++) {
     });
 }
 
+for (let i = 0; i < 100; i++) {
+  $(`#sbtm-${i+1}`).click(function(){
+    if(1){
+
+      $(`#token-error-${i+1}`).html(`
+      <div class="rounded-lg bg-error border border-3 border-danger">
+          <b class="mr-3">کد نامعتبر</b>
+          <p class="mr-2">برای دریافت کد ورود به آزمون باید به اکانت<span class="btn btn-link">تلگرام چوب خط</span>پیام دهید </p>
+      </div>
+      `);
+    }
+    else{
+
+    }
+  });
+}
