e5a92c75219eea4db60a8af5b2072e020e37634a
MahdiHGZ <58703084+MahdiHGZ@users.noreply.github.com>
2020-07-25 23:32:31 +0430

    Update front:index.js

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,26 +1,19 @@
-$(document).ready(function(){
-  var cards ="";
-  for (let i = 0; i < 100; i++) {
-
-    // if(i%6==0){
-    //   cards+=`
-    //   <div id="page-${((i-i%6)/6)+1}" class="row text-right main col-12 rtl ppage">
-    //   `
-    // }
-    
-    if(i%2==0){
-        var but_style="btn-primary"
-        var but="اضافه شدن به آزمون"
-    }
-    else{
-        var but_style="btn-danger"
-        var but="تمام شده"
-    }
-    cards += `
-    <div class="col-lg-4 col-sm-6 col-xs-12">
+function pageMaker(x,y,l){
+    var cards ="";
+    for (let i = x; i < x+y && i<l ; i++) {
+      if(i%2==0){
+          var but_style="btn-primary"
+          var but="اضافه شدن به آزمون"
+      }
+      else{
+          var but_style="btn-danger"
+          var but="تمام شده"
+      }
+      cards += `
+      <div class="col-lg-4 col-sm-6 col-xs-12">
           <div class="card  mb-1 mt-1 ml-0 mr-0 cardThem">
             <div class="card-body">
-              <h5 class="card-title">{exam_name}</h5>
+              <h5 class="card-title">{exam_name}${i+1}</h5>
               <p class="card-text">مخصوص پایه {exam_grade} </p>
               <p class="card-text">{qus_n}سواله</p>
               <p class="card-text">تاریخ برگزار: {exam_date}</p>
@@ -40,19 +33,15 @@ $(document).ready(function(){
              </div>
             </div>
           </div>
-     </div>
-    `
-    // if(i%6==0){
-    //   cards+=`</div>`
-    // }
-  }
+      </div>
+      `
+    }
 
-$("#exam-cards").append(cards);
+  $("#exam-cards").html(cards);
+}
 
-});
-
-
-for (let i = 0; i < 100; i++) {
+function btn_enter(x,y){
+  for (let i = x; i < x+y; i++) {
     $(`#btn-${i+1}`).click(function(){
       $(`.bg-error`).hide()
       for (let j = 0; j < 100; j++){
@@ -70,21 +59,66 @@ for (let i = 0; i < 100; i++) {
         else{
         }
     });
+  }
 }
 
-for (let i = 0; i < 100; i++) {
-  $(`#sbtm-${i+1}`).click(function(){
-    if(1){
-
-      $(`#token-error-${i+1}`).html(`
-      <div class="rounded-lg bg-error">
-          <b class="mr-4">کد نامعتبر</b>
-          <p class="ml-3 mr-3 error-t">برای دریافت کد ورود به آزمون باید به اکانت<a class="btn btn-link error-t-l mb-1 mr-0" href="https://t.me/choobkhat_admin">تلگرام چوب خط</a>پیام دهید</p>
-      </div>
-      `);
-    }
-    else{
-
-    }
-  });
+function verify_chek(x,y){
+  for (let i = x; i < x+y; i++) {
+    $(`#sbtm-${i+1}`).click(function(){
+      if(1){
+  
+        $(`#token-error-${i+1}`).html(`
+        <div class="rounded-lg bg-error">
+            <b class="mr-4">کد نامعتبر</b>
+            <p class="ml-3 mr-3 error-t">برای دریافت کد ورود به آزمون باید به اکانت<a class="btn btn-link error-t-l mb-1 mr-0" href="https://t.me/choobkhat_admin">تلگرام چوب خط</a>پیام دهید</p>
+        </div>
+        `);
+      }
+      else{
+  
+      }
+    });
+  }
 }
+
+function paginationMaker(z,l,y) {
+  pagin=` <li id="pagin-1" class="page-item"><a class="page-link" href="#">First</a></li>`
+  if(z>1 && z<(l+(y-l%y)%y)/y){ 
+    n=z-2;
+  }
+  else if(z==1){
+    n=0;
+  }
+  else{
+    n=(l+(y-l%y)%y)/y-3;
+  }
+  for (let i = n; i < n+3; i++) {
+    pagin+=`
+      <li id="pagin-${i+1}" class="page-item"><a class="page-link" href="#">${i+1}</a></li>
+    `
+  }
+  pagin+=` <li id="pagin-${(l+(y-l%y)%y)/y}" class="page-item"><a class="page-link" href="#">Last</a></li>`
+  $(`#pagination-bar`).html(pagin);
+}
+
+function paginBtn(l,y){
+  for (let i = 0; i < (l+(y-l%y)%y)/y; i++) {
+    $(`#pagin-${i+1}`).click(function(){
+        pageMaker((i)*y,y,l);
+        btn_enter((i)*y,y);
+        verify_chek((i)*y,y);
+        paginationMaker(i+1,l,y);
+        paginBtn(l,y);
+    });
+  }
+}
+
+
+$(document).ready(function(){
+  pageMaker(0,9,100);
+  btn_enter(0,9);
+  verify_chek(0,9);
+  paginationMaker(1,100,9);
+  paginBtn(100,9);
+});
+
